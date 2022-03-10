9102c652d996fa672113e041b60a64c3d8d5b03c
vugar <28726265+vugar005@users.noreply.github.com>
2022-03-10 23:39:13 +0100

    feature/improve-meta-tags (#6)

--- apps/shell/src/app.constants.ts
+++ apps/shell/src/app.constants.ts
@@ -1,1 +1,7 @@
+import { ICommonMetaTagConfig } from '@youtube/common-ui';
+
 export const APP_VERSION = '0.0.12';
+
+export const SHELL_META_TAGS: ICommonMetaTagConfig = {
+  title: 'Youtube Angular Clone',
+};
--- packages/common-ui/projects/ui/src/lib/models/index.ts
+++ packages/common-ui/projects/ui/src/lib/models/index.ts
@@ -3,3 +3,4 @@ export * from './youtube-search-list.model';
 export * from './youtube.service.model';
 export * from './mini-video-payload.model';
 export * from './country-code';
+export * from './meta-tag-config';
