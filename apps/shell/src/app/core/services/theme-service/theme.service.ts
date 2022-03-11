feb5529ba6bc68989567a3b03f761cbae5763321
vugar <vugar_abdullayev@outlook.com>
2022-03-11 15:20:55 +0100

    feature/save-selected-theme

--- apps/shell/src/app/core/services/theme-service/theme.service.ts
+++ apps/shell/src/app/core/services/theme-service/theme.service.ts
@@ -10,7 +10,7 @@ export class ThemeService {
   public setTheme(theme: AppTheme): void {
     const root = this.webApi.document.getElementsByTagName('html')[0];
     root.setAttribute('theme', theme);
-    this.webApi.localStorage.setItem(LocalStorageEnum.SAVED_THEME, JSON.stringify(theme));
     this.settingsStore.setTheme(theme);
+    this.webApi.localStorage.setItem(LocalStorageEnum.SAVED_THEME, JSON.stringify(theme));
   }
 }
