c8526e082e6a7fcbc809920d6f83b6c146cb7763
vugar <vugar_abdullayev@outlook.com>
2022-04-07 23:21:17 +0200

    patch app version

--- apps/shell/src/app.constants.ts
+++ apps/shell/src/app.constants.ts
@@ -1,6 +1,6 @@
 import { ICommonMetaTagConfig } from '@youtube/common-ui';
 
-export const APP_VERSION = '1.0.7';
+export const APP_VERSION = '1.0.9';
 
 export const SHELL_META_TAGS: ICommonMetaTagConfig = {
   title: 'Youtube Angular Clone',
