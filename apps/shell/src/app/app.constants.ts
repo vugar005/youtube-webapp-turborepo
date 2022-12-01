8ab5dfc6569829c6e5f8fb9b997227a17067f062
vugar <vugar_abdullayev@outlook.com>
2022-12-01 15:46:59 +0100

    fix keyboard shortcuts

--- apps/shell/src/app/app.constants.ts
+++ apps/shell/src/app/app.constants.ts
@@ -1,6 +1,6 @@
 import { ICommonMetaTagConfig } from '@youtube/common-ui';
 
-export const APP_VERSION = '3.1.0';
+export const APP_VERSION = '3.1.1';
 
 export const SHELL_META_TAGS: ICommonMetaTagConfig = {
   title: 'Youtube Angular Clone',
