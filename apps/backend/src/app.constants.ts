e0f0f2d064861609261e0a2df41f9cd6f85d7698
vugar <main@vugar.app>
2023-06-17 13:48:48 +0200

    fix backend cors issue for remote apps

--- apps/backend/src/app.constants.ts
+++ apps/backend/src/app.constants.ts
@@ -1,1 +1,8 @@
-export const WHITE_LIST_URLS: string[] = [undefined, 'https://youtube.vugar.app', 'http://localhost:4200'];
+export const WHITE_LIST_URLS: string[] = [
+  undefined,
+  'https://youtube.vugar.app',
+  'http://localhost:4200',
+  'http://localhost:4201',
+  'http://localhost:4202',
+  'http://localhost:4203',
+];
