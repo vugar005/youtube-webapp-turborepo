a9085c0f07cc049f6a0bb7dc723900a3671ee157
vugar <main@vugar.app>
2023-06-18 13:48:33 +0200

    bump app version

--- apps/shell/src/app/app.constants.ts
+++ apps/shell/src/app/app.constants.ts
@@ -1,6 +1,6 @@
 import { ICommonMetaTagConfig } from '@youtube/common-ui';
 
-export const APP_VERSION = '3.3.2';
+export const APP_VERSION = '3.3.4';
 
 export const SHELL_META_TAGS: ICommonMetaTagConfig = {
   title: 'Youtube Angular Clone',
