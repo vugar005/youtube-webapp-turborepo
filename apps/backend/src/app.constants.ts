c6bc4799ca9daa9ac33b42032e0c29aea089f193
vugar <main@vugar.app>
2023-08-04 18:05:17 +0200

    whitelist youtube-next

--- apps/backend/src/app.constants.ts
+++ apps/backend/src/app.constants.ts
@@ -1,6 +1,7 @@
 export const WHITE_LIST_URLS: string[] = [
   undefined,
   'https://youtube.vugar.app',
+  'https://youtube-next.vugar.app',
   'http://localhost:4200',
   'http://localhost:4201',
   'http://localhost:4202',
